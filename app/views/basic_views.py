05283450c28e5c48a3d115f69a2602aa120749b6
hyunji2776 <106853166+hyunji2776@users.noreply.github.com>
2023-07-12 09:46:03 +0900

    flask_0712 first commit

--- app/views/basic_views.py
+++ app/views/basic_views.py
@@ -7,17 +7,6 @@ fisa = Blueprint('basic',__name__, url_prefix='/fisa')
 #__name__ : flask 프레임워크가 찾을 이름
 #prefix : 라우팅 주소
 
-@fisa.route('/about_me')
-def about_me():
-    return f'저는 {__name__} 입니다'
-
-@fisa.route('/hello')
-def hello():
-    return f'안녕하세요'
-
-@fisa.route('/bye')
-def bye():
-    return f'잘 가세요'
 
 @fisa.route('/detail/<int:question_id>/')
 def detail(question_id):
